766bcf0219bb2ad05efc0be57e65bbb736e4fec6
Cole <coleshanefrancis@gmail.com>
2026-02-16 08:57:00 -0700

    Changed up generate dataset script to use a seprate config file

--- Dataset/generate_dataset.py
+++ Dataset/generate_dataset.py
@@ -1,54 +1,62 @@
-import numpy as np
+import json
 import random
+import shutil
+from pathlib import Path
+import numpy as np
 from scipy import signal
 
-SAMPLE_RATE = 44100
-NUM_SAMPLES = 256# 1024
-# A4 E5 A5 E6 A6 E7
-FREQS = [440.0, 659.3, 880.0, 1318.5, 1760.0, 2637.0]
+config_file = "./generate_dataset_config.json"
 
-MIN_AMPLITUDE = -1.0
-MAX_AMPLITUDE = 1.0
+with open(config_file, "r") as f:
+    configs = json.load(f)
 
-WAVE_TYPE = ['sine', 'square', 'triange', 'saw', 'noise']
+base_dir = Path(configs["datasetConfigs"]["datasetName"])
 
-t = np.arange(NUM_SAMPLES) / SAMPLE_RATE
+if base_dir.exists():
+    shutil.rmtree(base_dir)
 
-def generate_random_sample(wave_type):
-    freq = FREQS[np.random.randint(0, len(FREQS)-1)]
+for split in ["train", "test"]:
+    (base_dir / split / "samples").mkdir(parents=True, exist_ok=True)
+    (base_dir / split / "labels").mkdir(parents=True, exist_ok=True)
 
-    amplitude = np.random.uniform(MIN_AMPLITUDE, MAX_AMPLITUDE)
+def generate_random_sample(times, wave_type, signalConfigs):
+    freq = signalConfigs["frequencies"][np.random.randint(0, len(signalConfigs["frequencies"])-1)]
+
+    amplitude = np.random.uniform(signalConfigs["minAmplitude"], signalConfigs["maxAmplitude"])
 
     phase = np.random.uniform(0, 2*np.pi)
 
     match wave_type:
         case 'sine':
-            x = amplitude * np.sin(2*np.pi * freq * t + phase)
+            x = amplitude * np.sin(2*np.pi * freq * times + phase)
 
         case 'square':
-            x = amplitude * signal.square(2*np.pi * freq * t + phase)
+            x = amplitude * signal.square(2*np.pi * freq * times + phase)
 
         case 'triange':
-            x = amplitude * signal.sawtooth(2*np.pi * freq * t + phase, width=0.5)
+            x = amplitude * signal.sawtooth(2*np.pi * freq * times + phase, width=0.5)
 
         case 'saw':
-            x = amplitude * signal.sawtooth(2*np.pi * freq * t + phase)
+            x = amplitude * signal.sawtooth(2*np.pi * freq * times + phase)
 
         case 'noise':
-            x = amplitude * np.array([random.random() for _ in range(len(t))])
+            x = amplitude * np.array([random.random() for _ in range(len(times))])
 
         case _:
-            x = np.zeros_like(t)
+            x = np.zeros_like(times)
 
     # add noise
-    noise = np.random.normal(0, 0.01, size= x.shape)
+    noise = np.random.normal(0, signalConfigs["noiseScale"], size= x.shape)
 
     return x + noise
 
 
 import matplotlib.pyplot as plt
 
-sig = generate_random_sample(WAVE_TYPE[4])
+
+t = np.arange(configs["signalConfigs"]["numSamples"]) / configs["signalConfigs"]["sampleRate"]
+
+sig = generate_random_sample(t, configs["signalConfigs"]["signalTypes"][0], configs["signalConfigs"])
 
 plt.plot(t, sig)
 plt.show()
